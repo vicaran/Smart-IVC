ebf90e4d00d471d328c7c20949c03f25f1527ce0
vicaran <andreavicari95@gmail.com>
2017-03-31 19:23:50 +0200

    removing sensitive data

--- src/main/java/com/app/utils/APIKeys.java
+++
@@ -1,8 +0,0 @@
-package com.app.utils;
-
-/**
- * Created by Andrea on 15/03/2017.
- */
-public class APIKeys {
-    public static final String GoogleMapsKey = "AIzaSyCY1ov7-HWE-CahFZyIjaGOQYGYK-T8wls";
-}

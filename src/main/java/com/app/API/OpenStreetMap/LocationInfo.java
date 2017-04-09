25a2991925ad47f85e5cbf539192f4a05d73f7d2
vicaran <andreavicari95@gmail.com>
2017-04-09 15:36:36 +0200

    logo updated + favicons

--- src/main/java/com/app/API/OpenStreetMap/LocationInfo.java
+++ src/main/java/com/app/API/OpenStreetMap/LocationInfo.java
@@ -56,7 +56,7 @@ public class LocationInfo implements OpenStreetMapAPIServices {
 
         String prepender = ",";
 
-        File jsonFile = new File("src/main/resources/city_data/OSM_DATA3.json");
+        File jsonFile = new File("src/main/resources/city_data/OSM_DATA4.json");
         if(!jsonFile.exists()){
             prepender = "[";
         }
